5a87100498e3ef7a0a2c3c937ebb4cd97747cb62
Louis <Louis@LAPTOP-TH69ARB6>
2024-05-20 13:46:11 -0400

    Agregar archivos de proyecto.

---
+++ Numeros/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MayorMenor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce el primer número:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Introduce el segundo número:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Introduce el tercer número:");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            int mayor = num1;
+            int menor = num1;
+
+            if (num2 > mayor)
+            {
+                mayor = num2;
+            }
+            if (num3 > mayor)
+            {
+                mayor = num3;
+            }
+
+            if (num2 < menor)
+            {
+                menor = num2;
+            }
+            if (num3 < menor)
+            {
+                menor = num3;
+            }
+
+            Console.WriteLine("El número mayor es: " + mayor);
+            Console.WriteLine("El número menor es: " + menor);
+        }
+    }
+}
